3417eaddca78b08c84ae1d4ad157004039f536bf
Alberto Motta <amotta@digitalidentity.tech>
2019-09-17 17:17:15 +0200

    optimizing components

--- src/components/Education.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Timeline from './Timeline'
-import Section from "./Section"
-
-export default ({ even, items, ...props }) => (
-  <Section title="Education" even={even} {...props}>
-    <Timeline items={items} even={even} />
-  </Section>
-) 
--- src/components/Technologies.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Grid from './Grid'
-import Section from "./Section"
-
-export default props => (
-  <Section title="Technologies" {...props}>
-    <Grid />
-  </Section>
-)
